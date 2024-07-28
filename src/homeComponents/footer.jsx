f92c0e82af12bdc08d764d6fe3fabf4500b6db96
Sujal129 <139127329+Sujal129@users.noreply.github.com>
2024-07-28 18:47:27 +0530

    Add files via upload

---
+++ src/homeComponents/footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./style.css";
+class Footer extends Component {
+  render() {
+    return (
+      <div className="greet">
+        <hr className="dropdown-divider" />
+        <h7>&copy; Sujal Gupta 2024.</h7>
+        <hr className="dropdown-divider" />
+      </div>
+    );
+  }
+}
+
+export default Footer;
